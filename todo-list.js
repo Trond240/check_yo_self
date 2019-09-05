822f61523201ed9020fddb3fd89255ab41e0cb31
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-09-05 10:35:25 -0600

    Create skeleton for todo-list.js and task.js

--- todo-list.js
+++ todo-list.js
@@ -1,6 +1,25 @@
 class toDoList {
-  constructor() {
-// should have a unique identifier -- can find out what time/date something happened
+  constructor(obj) {
+    obj.id = id;
+// should have a unique identifier -- can find out what time/date something happened -- timestamp?
+    obj.title = title;
+    obj.urgent = false
 // pass in the unique ID when instantiating an object as an argument
+    obj.tasks = [];
+  }
+  saveToStorage() {
+
+  }
+  deleteFromStorage() {
+
+  }
+  updateToDo(){
+    //should update object's title and urgency
+  }
+  updateTask() {
+    //should update task's content and if it's been completed
+    obj.tasks.push(task.text);
   }
 }
+
+var teacher = 'Amy';
