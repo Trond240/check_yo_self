f57f227ac3ae7f0597a954fd42eff62e4518bbd0
Kayla Wood <kaylaewood@gmail.com>
2019-09-09 20:58:34 -0600

    Refactor all code for readability

--- todo-list.js
+++ todo-list.js
@@ -4,17 +4,19 @@ class ToDoList {
     this.title = obj.title;
     this.tasks = obj.tasks;
   }
+
   saveToStorage() {
-
   }
+
   deleteFromStorage() {
-
   }
+
   updateToDo(){
     //should update object's title and urgency
   }
+
   updateTask(event) {
     var taskContainer = event.target.parentNode;
-
     }
-  }
+
+  };
